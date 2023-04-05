a0057a7bdfb1f6ef74503e8c3bda63dcd6ee5742
Claudia236 <panza.claudia23@gmail.com>
2023-04-05 16:10:02 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.lang.Integer;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /* METODI
+            blocchi di codice che è possibile richiamare (funzioni)
+
+         */
+
+
+
+
+    }
+}
